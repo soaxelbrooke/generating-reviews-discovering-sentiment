d4c47a09007d6404d375ef5b1118f00f5d0cad3a
soaxelbrooke <stuart@axelbrooke.com>
2017-11-23 06:39:20 -0800

    Add basic setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+# coding=utf-8
+
+from setuptools import setup
+
+setup(name='sentiment_neuron',
+      version='0.0.1',
+      packages=['sentiment_neuron'],
+      install_requires=[],
+      zip_safe=False)
+
